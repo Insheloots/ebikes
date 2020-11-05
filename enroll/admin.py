2887d167a45e8e52ca7a80881f3b48f5f8a44484
Oh-Snap-Itz-Ari <alexfabianmelo12@hotmail.com>
2020-11-05 12:39:19 -0500

    Migraciones completadas

--- enroll/admin.py
+++ enroll/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Propietario, Proveedor, Tipo_Trabajador, Trabajador
+from .models import Product, Propietario, Proveedor, Tipo_Trabajador, Trabajador, Salario_Trabajador, Tipo_Producto, Mensajero, Ciudad, Comprador, Factura, Especificacion_Factura
 
 # Register your models here.
 @admin.register(Product)
@@ -8,11 +8,11 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Propietario)
 class PropietarioAdmin(admin.ModelAdmin):
-    list_display = ('cedula_propietario', 'nombre', 'apellido','correo', 'telefono', 'fecha_nacimiento')
+    list_display = ('cedula_propietario', 'nombre', 'apellido','correo_electronico', 'direccion', 'telefono', 'fecha_nacimiento')
 
 @admin.register(Proveedor)
 class ProveedorAdmin(admin.ModelAdmin):
-    list_display = ('id_proveedor','cedula_propietario', 'nombre_proveedor','telefono', 'correo')
+    list_display = ('id_proveedor','cedula_propietario', 'nombre_proveedor', 'direccion','telefono', 'correo_electronico')
 
 @admin.register(Tipo_Trabajador)
 class Tipo_TrabajadorAdmin(admin.ModelAdmin):
@@ -20,4 +20,32 @@ class Tipo_TrabajadorAdmin(admin.ModelAdmin):
 
 @admin.register(Trabajador)
 class TrabajadorAdmin(admin.ModelAdmin):
-    list_display = ('cedula_trabajador', 'id_tipo', 'nombre', 'apellido','correo', 'telefono', 'fecha_nacimiento')
+    list_display = ('cedula_trabajador', 'id_tipo', 'nombre', 'apellido','correo_electronico', 'direccion', 'telefono', 'fecha_nacimiento')
+
+@admin.register(Salario_Trabajador)
+class Salario_TrabajadorAdmin(admin.ModelAdmin):
+    list_display = ('id_salario', 'sueldo_base', 'honorarios', 'seguro_social', 'pensiones', 'sueldo_total')
+
+@admin.register(Tipo_Producto)
+class Tipo_ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id_tipo', 'nombre_tipo')
+
+@admin.register(Mensajero)
+class MensajeroAdmin(admin.ModelAdmin):
+    list_display = ('cedula_mensajero', 'nombre', 'apellido','correo_electronico', 'direccion', 'telefono', 'fecha_nacimiento')
+
+@admin.register(Ciudad)
+class CiudadAdmin(admin.ModelAdmin):
+    list_display = ('id_ciudad', 'nombre_ciudad')
+
+@admin.register(Comprador)
+class CompradorAdmin(admin.ModelAdmin):
+    list_display = ('cedula_comprador', 'nombre', 'apellido','correo_electronico', 'id_ciudad', 'direccion', 'telefono', 'fecha_nacimiento')
+
+@admin.register(Factura)
+class FacturaAdmin(admin.ModelAdmin):
+    list_display = ('id_factura', 'id', 'cedula_comprador', 'cedula_mensajero', 'fecha_emision', 'subtotal', 'iva', 'total')
+
+@admin.register(Especificacion_Factura)
+class Especificacion_FacturaAdmin(admin.ModelAdmin):
+    list_display = ('id_especificacion', 'id', 'id_factura', 'cantidad_compra', 'importe')
--- enroll/models.py
+++ enroll/models.py
@@ -5,7 +5,8 @@ class Propietario(models.Model):
     cedula_propietario = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=70)
     apellido = models.CharField(max_length=70)
-    correo = models.EmailField(max_length=70)
+    correo_electronico = models.EmailField(max_length=70)
+    direccion = models.CharField(max_length=70)
     telefono = models.IntegerField()
     fecha_nacimiento = models.DateField()
 
@@ -13,8 +14,9 @@ class Proveedor(models.Model):
     id_proveedor = models.AutoField(auto_created=True, primary_key=True)
     cedula_propietario = models.ForeignKey(Propietario, on_delete=models.CASCADE)
     nombre_proveedor = models.CharField(max_length=70)
+    direccion = models.CharField(max_length=70)
     telefono = models.IntegerField()
-    correo = models.EmailField(max_length=70)
+    correo_electronico = models.EmailField(max_length=70)
 
 class Tipo_Trabajador(models.Model):
     id_tipo = models.AutoField(auto_created=True, primary_key=True)
@@ -25,12 +27,67 @@ class Trabajador(models.Model):
     id_tipo = models.ForeignKey(Tipo_Trabajador, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=70)
     apellido = models.CharField(max_length=70)
-    correo = models.EmailField(max_length=70)
+    correo_electronico = models.EmailField(max_length=70)
+    direccion = models.CharField(max_length=70)
     telefono = models.IntegerField()
     fecha_nacimiento = models.DateField()
 
+class Salario_Trabajador(models.Model):
+    id_salario = models.AutoField(auto_created=True, primary_key=True)
+    sueldo_base = models.FloatField()
+    honorarios = models.FloatField()
+    seguro_social = models.FloatField()
+    pensiones = models.FloatField()
+    sueldo_total = models.FloatField()
+
+class Tipo_Producto(models.Model):
+    id_tipo = models.AutoField(auto_created=True, primary_key=True)
+    nombre_tipo = models.CharField(max_length=70)
+
 class Product(models.Model):
+    #id_tipo = models.ForeignKey(Tipo_Producto, on_delete=models.CASCADE)
+    #id_proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
     nombre_producto = models.CharField(max_length=70)
     cantidad_producto = models.PositiveIntegerField()
     descripcion_producto = models.CharField(max_length=300)
-    precio_unit = models.FloatField(max_length=20)
+    precio_unit = models.FloatField()
+
+class Mensajero(models.Model):
+    cedula_mensajero = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=70)
+    apellido = models.CharField(max_length=70)
+    correo_electronico = models.EmailField(max_length=70)
+    direccion = models.CharField(max_length=70)
+    telefono = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+
+class Ciudad(models.Model):
+    id_ciudad = models.AutoField(auto_created=True, primary_key=True)
+    nombre_ciudad = models.CharField(max_length=70)
+
+class Comprador(models.Model):
+    id_ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+    cedula_comprador = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=70)
+    apellido = models.CharField(max_length=70)
+    correo_electronico = models.EmailField(max_length=70)
+    direccion = models.CharField(max_length=70)
+    telefono = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+
+class Factura(models.Model):
+    id_factura = models.AutoField(auto_created=True, primary_key=True)
+    id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cedula_comprador = models.ForeignKey(Comprador, on_delete=models.CASCADE)
+    cedula_mensajero = models.ForeignKey(Mensajero, on_delete=models.CASCADE)
+    fecha_emision = models.DateTimeField(auto_now_add=True)
+    subtotal = models.FloatField()
+    iva = models.FloatField()
+    total = models.FloatField()
+
+class Especificacion_Factura(models.Model):
+    id_especificacion = models.AutoField(auto_created=True, primary_key=True)
+    id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    id_factura = models.ForeignKey(Factura, on_delete=models.CASCADE)
+    cantidad_compra = models.IntegerField()
+    importe = models.FloatField()
