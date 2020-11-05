e20ec2ab27f6626ff9f8c05de1805efa6d75bfee
Oh-Snap-Itz-Ari <alexfabianmelo12@hotmail.com>
2020-11-05 11:19:48 -0500

    Mitad de migraciones

--- enroll/admin.py
+++ enroll/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Propietario, Proveedor, Tipo_Trabajador, Trabajador
 
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre_producto', 'cantidad_producto','descripcion_producto', 'precio_unit')
+
+@admin.register(Propietario)
+class PropietarioAdmin(admin.ModelAdmin):
+    list_display = ('cedula_propietario', 'nombre', 'apellido','correo', 'telefono', 'fecha_nacimiento')
+
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ('id_proveedor','cedula_propietario', 'nombre_proveedor','telefono', 'correo')
+
+@admin.register(Tipo_Trabajador)
+class Tipo_TrabajadorAdmin(admin.ModelAdmin):
+    list_display = ('id_tipo','tipo_trabajador')
+
+@admin.register(Trabajador)
+class TrabajadorAdmin(admin.ModelAdmin):
+    list_display = ('cedula_trabajador', 'id_tipo', 'nombre', 'apellido','correo', 'telefono', 'fecha_nacimiento')
--- enroll/models.py
+++ enroll/models.py
@@ -1,6 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Propietario(models.Model):
+    cedula_propietario = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=70)
+    apellido = models.CharField(max_length=70)
+    correo = models.EmailField(max_length=70)
+    telefono = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+
+class Proveedor(models.Model):
+    id_proveedor = models.AutoField(auto_created=True, primary_key=True)
+    cedula_propietario = models.ForeignKey(Propietario, on_delete=models.CASCADE)
+    nombre_proveedor = models.CharField(max_length=70)
+    telefono = models.IntegerField()
+    correo = models.EmailField(max_length=70)
+
+class Tipo_Trabajador(models.Model):
+    id_tipo = models.AutoField(auto_created=True, primary_key=True)
+    tipo_trabajador = models.CharField(max_length=70)
+
+class Trabajador(models.Model):
+    cedula_trabajador = models.IntegerField(primary_key=True)
+    id_tipo = models.ForeignKey(Tipo_Trabajador, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=70)
+    apellido = models.CharField(max_length=70)
+    correo = models.EmailField(max_length=70)
+    telefono = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+
 class Product(models.Model):
     nombre_producto = models.CharField(max_length=70)
     cantidad_producto = models.PositiveIntegerField()
